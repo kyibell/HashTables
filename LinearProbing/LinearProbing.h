a045c28edacfc48b73cfbd863e0f8c65181aca15
kyibell <bellkyilee@gmail.com>
2024-12-02 12:25:18 -0600

    Implemented Table class and Record Struct.:

---
+++ LinearProbing/LinearProbing.h
@@ -0,0 +1,43 @@
+
+#ifndef LINEAR_PROBING_H
+#define LINEAR_PROBING_H
+
+
+
+const int CAPACITY = 31; // Set a const of capacity = 31
+
+
+struct Record { // Structs for Records 
+    int key;
+    int data;
+};
+
+
+class Table {
+    public:
+    Table(); // Constructor to initialize table.
+    void insert(const Record &entry);
+    void erase(int key);
+    void find(int key, bool found, Record &result) const;
+    int size() const;
+
+    // Hashing Methods
+    void DoubleHash(int key);
+    void MidSquareHash(int key);
+    void ModuloHash(int key);
+
+    //Probing Hash Methods:
+    void LinearProbing(int key);
+    void QuadraticProbing(int key);
+
+
+    void print(); // print function
+
+    private:
+    Record table[CAPACITY];
+    int used;
+
+}; 
+
+
+#endif // LINEARPROBING.H
